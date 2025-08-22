76c40572af9110dc2443429dc8a1bcce0c465cd2
Sungmin Lee <sungminlee417@gmail.com>
2025-08-21 20:28:08 -0700

    fix: add studio as base url

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,7 @@ import { schemaTypes } from "./schemas";
 export default defineConfig({
   name: "default",
   title: "Glenn Canin Guitars CMS",
+  basePath: "/studio",
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
